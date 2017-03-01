7ace197a37eb3676748717f24ab6cd5470141d45
shilpavijay <shilpa.amberker@gmail.com>
2017-03-02 00:21:14 +0530

    'buildingDS'

---
+++ BuildDataset3.py
@@ -0,0 +1,47 @@
+import quandl
+import pandas as pd
+import pickle
+
+api_key = open('quandlapikey.txt','r').read()
+
+# df = quandl.get("FMAC/HPI_AZ", authtoken=api_key)
+# print(df.head())
+
+
+
+def state_names():
+	states_list = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+	return states_list[0][0][1:]
+
+
+def get_all_data():
+	main_df = pd.DataFrame()
+	states = state_names()
+	for abbv in states:
+		query = "FMAC/HPI_" + str(abbv)
+		df = quandl.get(query,authtoken=api_key)
+		df.rename(columns={'Value':abbv},inplace=True)
+		# print(df.head())
+
+		if main_df.empty:
+			main_df = df
+		else:
+			main_df = main_df.join(df)
+
+	pickle_out = open('states_HPI.pickle','wb')
+	pickle.dump(main_df,pickle_out)
+	pickle_out.close()
+
+# get_all_data()
+
+pickle_in = open('states_HPI.pickle','rb')
+HPI = pickle.load(pickle_in)
+print(HPI)
+
+#Pandas has its own pickle:
+
+# HPI_data.to_pickle('pickle.pickle')
+# HPI_data2 = pd.read_pickle('pickle.pickle')
+# print(HPI_data2)
+
+
