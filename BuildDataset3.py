dc8feed32c8772298d39bc5bbe3eeb7e57df69f2
shilpavijay <shilpa.amberker@gmail.com>
2017-03-22 18:21:02 +0530

    index

--- BuildDataset3.py
+++ BuildDataset3.py
@@ -1,6 +1,9 @@
 import quandl
 import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('fivethirtyeight')
 
 api_key = open('quandlapikey.txt','r').read()
 
@@ -34,14 +37,26 @@ def get_all_data():
 
 # get_all_data()
 
-pickle_in = open('states_HPI.pickle','rb')
-HPI = pickle.load(pickle_in)
-print(HPI)
+# pickle_in = open('states_HPI.pickle','rb')
+# HPI = pickle.load(pickle_in)
+# print(HPI)
 
-#Pandas has its own pickle:
+########## Pandas has its own pickle:
 
 # HPI_data.to_pickle('pickle.pickle')
 # HPI_data2 = pd.read_pickle('pickle.pickle')
 # print(HPI_data2)
 
+HPI_data = pd.read_pickle('states_HPI.pickle')
+
+########## Changing column data ###############
+
+# HPI_data['AZ2'] = HPI_data['AZ'] * 2
+# print(HPI_data[['AZ','AZ2']])
+
+########## plotting data using Matplotlib ####
+
+HPI_data.plot()
+plt.legend().remove()
+plt.show()
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Working with Pandas and Numpy for Data Analysis
+This repository contains essentials of PANDAS and NUMPY for Basic Data Analysis. 
+Here is a list:
+
+- Introduction to Creating and working with Data Frames
+- Handling Data from Quandl/CSV
+- Building the Dataset
+- Pickle
+- Joining, Merging, Concatinating and Appending Data Frames
